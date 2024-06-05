50a29860c7e92a79bcb7bd7b4744af9f5ac782f7
TonyGrif <TonyGriffin2000@gmail.com>
2024-06-05 11:22:24 -0400

    feat: take in example prompt

--- src/main/java/GearRatios.java
+++ src/main/java/GearRatios.java
@@ -1,5 +1,20 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class GearRatios {
     public static void main (String[] args) {
-        System.out.println("Hello World!");
+        String filePath = System.getProperty("user.dir") + "/src/resources/example.txt";
+        Scanner reader = null;
+
+        try {
+            reader = new Scanner(new FileReader(filePath));
+        } catch (FileNotFoundException e) {
+            System.out.printf("File not found at %s%n", filePath);
+            System.exit(-1);
+        }
+        while (reader.hasNextLine()) {
+            System.out.println(reader.nextLine());
+        }
     }
 }
